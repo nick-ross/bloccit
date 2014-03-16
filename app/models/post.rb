71162a9b1dff3cfef243af2907f938a86c9723fb
Nick Ross <mx5433@gmail.com>
2014-03-15 19:09:10 -0500

    post picture fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :body, :title, :topic, :image
+  attr_accessible :body, :title, :topic, :image, :image_cache
   has_many :comments
   belongs_to :user
   belongs_to :topic
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :avatar
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :avatar, :avatar_cache
   has_many :posts
   has_many :comments
   before_create :set_member
