c8f0a8c15fb847debdd17fc108545f405d3b6821
Nick Ross <mx5433@gmail.com>
2014-03-19 12:02:29 -0500

    Interlude changes for registration, email permissions and styling

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,9 +5,18 @@ class Vote < ActiveRecord::Base
   validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote." }
   after_save :update_post
 
+  def up_vote?
+    value == 1
+  end
+
+  def down_vote?
+    value == -1
+  end
+  
   private
 
   def update_post
     self.post.update_rank  
   end
+
 end
