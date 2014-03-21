9551e1701da16b3af0d2c0e682c6beae72468f43
Nick Ross <mx5433@gmail.com>
2014-03-20 22:00:53 -0500

    Created Popular Posts and Users, and optimized topics#show with eager loading.

--- app/controllers/topics/posts_controller.rb
+++ app/controllers/topics/posts_controller.rb
@@ -1,8 +1,4 @@
-class PostsController < ApplicationController
-  def index
-    @posts = Post.visible_to(current_user).where("posts.created_at > ?",7.days.ago).paginate(page: params[:page], per_page:10)
-  end
-  
+class Topics::PostsController < ApplicationController
   def show
     @topic = Topic.find(params[:topic_id])
     authorize! :read, @topic, message: "You need to be signed-in to do that."
